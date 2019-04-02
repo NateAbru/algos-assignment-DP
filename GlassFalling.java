9b4f303f4048b2bc975575a47152dbf0a7d6d7f9
NateAbru <40753116+NateAbru@users.noreply.github.com>
2019-04-02 16:47:43 -0400

    Add GlassFalling and RodCutting

--- GlassFalling.java
+++ GlassFalling.java
@@ -1,38 +1,128 @@
 /**
+
  * Glass Falling
+
  */
+
 public class GlassFalling {
 
+
+
   // Do not change the parameters!
+
   public int glassFallingRecur(int floors, int sheets) {
-    // Fill in here and change the return
-    return 0;
+     
+	 //Base Case
+	 //if there's no floors no trials are possible since there are no floors to drop the glass from
+	 //if there's only one floor only one trial can be done
+	 //if we only have one sheet we need to check each floor starting from 1,
+	 //assuming worst case scenario if each floor does not cause the sheet
+	 //to break then with one sheet the number of trials would be the 
+	 //number of floors
+	 if(floors == 0 || floors == 1 || sheets == 1)
+	 {
+		 return floors;
+	 }
+	
+	 int min = floors;
+	 int trialMin;
+	 for(int i = 1; i <= floors; i++)
+	 {
+		if(glassFallingRecur(i - 1, sheets - 1) < glassFallingRecur(floors - i, sheets))
+		{
+			trialMin = glassFallingRecur(floors - i, sheets);
+		}
+		else trialMin = glassFallingRecur(i - 1, sheets - 1);
+		if(trialMin < min) min = trialMin;
+	 }
+    return min + 1;
   }
+
+
 
   // Optional:
+
   // Pick whatever parameters you want to, just make sure to return an int.
+
   public int glassFallingMemoized() {
+
     // Fill in here and change the return
+
     return 0;
+
   }
 
+
+
   // Do not change the parameters!
+
   public int glassFallingBottomUp(int floors, int sheets) {
-    // Fill in here and change the return
-    return 0;
+	  
+    //create an array to store the memoized values
+    //use #of floors and sheets for the double dimension array
+	//and add 1 or out of bounds exception will be declared due to for loop bounds
+	int[][] glassArray = new int[floors+1][sheets+1];
+	int trialMin;
+	
+	//stores the value of 1 for when there is only 1 floor as there
+	//could only be 1 trial in such a case and
+	//0 when there are no floors since we cannot drop any sheet
+	for(int s = 1; s <= sheets; s++)
+	{
+		glassArray[1][s] = 1;
+		glassArray[0][s] = 0;
+	}
+	//stores the value of # of floors when there is only 1 sheet
+	//since the worst case scenario would be to check 
+	//each floor from the bottom up when there is only 1 sheet
+    for(int f = 1; f <= floors; f++)
+    {
+    	glassArray[f][1] = f;
+    }
+    //this for loop stores the trial # values in the remaining indexes
+    for(int s2 = 2; s2 <= sheets; s2++) 
+    {
+    	for(int f2 = 2; f2 <= floors; f2++)
+    	{
+    		glassArray[f2][s2] = floors;
+    		for(int i = 1; i <= f2; i++)
+    		{
+    			if(glassArray[i - 1][s2 - 1] < glassArray[f2-i][s2])
+    			{
+    				trialMin = 1 + glassArray[f2-i][s2];
+    			}
+    			else trialMin = 1 + glassArray[i-1][s2 - 1];
+    			if(trialMin < glassArray[f2][s2]) glassArray[f2][s2] = trialMin;
+    		}
+    	}
+    }
+    return glassArray[floors][sheets];
   }
+
+
+
 
 
   public static void main(String args[]){
-      GlassFalling gf = new GlassFalling();
+	  
+	  GlassFalling gf = new GlassFalling();
 
       // Do not touch the below lines of code, and make sure
+
       // in your final turned-in copy, these are the only things printed
+
       int minTrials1Recur = gf.glassFallingRecur(27, 2);
+
       int minTrials1Bottom = gf.glassFallingBottomUp(27, 2);
+
       int minTrials2Recur = gf.glassFallingRecur(100, 3);
+
       int minTrials2Bottom = gf.glassFallingBottomUp(100, 3);
+
       System.out.println(minTrials1Recur + " " + minTrials1Bottom);
+
       System.out.println(minTrials2Recur + " " + minTrials2Bottom);
+
   }
+
 }
--- RodCutting.java
+++ RodCutting.java
@@ -1,33 +1,90 @@
 /**
+
  * Rod cutting problem described in Chapter 15 of textbook
+
  */
+
 public class RodCutting {
 
-  // Do not change the parameters!
-  public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
-    return 0;
+
+  public int max(int x, int y)
+  {
+	  if(x > y) return x;
+	  return y;
   }
 
   // Do not change the parameters!
-  public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
-    return 0;
+
+  public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
+	//base Case
+	//if there is no rod length to cut the price will be 0
+	if(rodLength <= 0)
+	{
+		return 0;
+	}
+	int max = -1;
+	for(int i = 0; i < rodLength; i++)
+	{
+		max = max(max,lengthPrices[i] + rodCuttingRecur(rodLength - i - 1, lengthPrices));
+	}
+    return max;
   }
+
+
+
+  // Do not change the parameters!
+
+  public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
+	  
+	  int[] newArray = new int[rodLength + 1];
+	  newArray[0] = 0;
+	  
+	  for (int i = 1; i <= rodLength; i++)
+	  {
+		  int max = -1;
+		  for(int c = 0; c < i; c++)
+		  {
+			  max = max(max, lengthPrices[c] + newArray[i-c-1]);
+		  }
+		  newArray[i] = max;
+	  }
+
+    return newArray[rodLength];
+
+  }
+
+
+
 
 
   public static void main(String args[]){
-      RodCutting rc = new RodCutting();
+
+	  RodCutting rc = new RodCutting();
 
       // In your turned in copy, do not touch the below lines of code.
+
       // Make sure below is your only output.
+
       int length1 = 7;
+
       int[] prices1 = {1, 4, 7, 3, 19, 5, 12};
+
       int length2 = 14;
+
       int[] prices2 = {2, 5, 1, 6, 11, 15, 17, 12, 13, 9, 10, 22, 18, 26};
+
       int maxSell1Recur = rc.rodCuttingRecur(length1, prices1);
+
       int maxSell1Bottom = rc.rodCuttingBottomUp(length1, prices1);
+
       int maxSell2Recur = rc.rodCuttingRecur(length2, prices2);
+
       int maxSell2Bottom = rc.rodCuttingBottomUp(length2, prices2);
+
       System.out.println(maxSell1Recur + " " + maxSell1Bottom);
+
       System.out.println(maxSell2Recur + " " + maxSell2Bottom);
+
   }
+
 }
